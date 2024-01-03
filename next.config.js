cf1e695538f3d1e56a858394faab3b61555ce9be
Dmsooja <dev@sooja.fr>
2024-01-03 11:17:59 +0100

    update config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = async () => {
+  return {
+    images: {
+      loader: "imgix",
+      path: "https://images.prismic.io/",
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+  };
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
