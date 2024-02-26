df2dc7b92372bc9afa232053daba06c384a14dd6
Dmsooja <dev@sooja.fr>
2024-02-26 11:49:22 +0100

    Create featured partners slice

--- slices/FeaturedPartners/index.tsx
+++ slices/FeaturedPartners/index.tsx
@@ -1,13 +1,33 @@
 import React from "react";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
-import { PrismicRichText } from "@prismicio/react";
 import { SliceComponentProps } from "@prismicio/react";
-import { ChevronRightIcon } from "@heroicons/react/solid";
+import { PrismicRichText } from "@/components/PrismicRichText";
+
+const serializer = {
+  heading3: ({ children }) => (
+    <h3
+      // className="font-sans font-bold text-3xl text-[#171717] mb-8"
+      // className="text-4xl font-sans font-bold text-[#171717] text-center mb-4"
+      className="text-xl font-sans font-bold text-[#171717] mb-4"
+    >
+      {children}
+    </h3>
+  ),
+  paragraph: ({ children }) => (
+    <p
+      // className="mb-4 text-[#171717] font-sans text-lg"
+      className="font-sans text-lg text-[#171717] max-w-2xl text-center mx-auto mb-[88px]"
+    >
+      {children}
+    </p>
+  ),
+};
 
 /**
  * Props for `FeaturedPartners`.
  */
-export type FeaturedPartnersProps = SliceComponentProps<Content.FeaturedPartnersSlice>;
+export type FeaturedPartnersProps =
+  SliceComponentProps<Content.FeaturedPartnersSlice>;
 
 /**
  * Component for "FeaturedPartners" Slices.
@@ -15,44 +35,40 @@ export type FeaturedPartnersProps = SliceComponentProps<Content.FeaturedPartners
 const FeaturedPartners = ({ slice }: FeaturedPartnersProps): JSX.Element => {
   return (
     <section className="bg-white">
-      <div className="max-w-screen-xl mx-auto py-12 px-6 sm:px-8 lg:py-24">
-        <PrismicRichText
-          field={slice.primary.section_title}
-          components={{
-            heading2: ({ children }) => (
-              <h2 className="text-center text-[48px] leading-[52px] md:text-[64px] md:leading-[72px] font-bold tracking-[-0.03em] text-[#151515] mb-12">
-                {children}
-              </h2>
-            ),
-          }}
-        />
-
-        <div className="flex justify-center items-center">
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-8">
-            {slice.items.map((item, index) => (
-              <PrismicNextLink
-                key={index}
-                field={item.partner_link}
-                className="transition-transform hover:scale-105">
-                <div className="p-4 rounded-lg h-24 w-52 object-fill flex justify-center items-center shadow-sm">
-                  <PrismicNextImage
-                    field={item.partner_logo}
-                    height={96}
-                  />
+      <div className="flex flex-col items-center my-16 mx-auto max-w-full">
+          <PrismicRichText field={slice.primary.section_title} />
+          <PrismicRichText field={slice.primary.section_description} />
+          <div className="px-4">
+            <div
+              className="flex flex-row gap-8 overflow-x-auto scroll-smooth scrollbar-hide"
+              style={{ scrollSnapType: "x mandatory" }}
+            >
+              {slice.items.map((item, idx) => (
+                <div
+                  key={idx}
+                  className="flex-none w-1/5 min-w-1/5"
+                  style={{ scrollSnapAlign: "start" }}
+                >
+                  <PrismicNextLink
+                    className="block h-24 rounded-lg overflow-hidden bg-[#171717] cursor-pointer transform hover:scale-110 transition-transform duration-300"
+                    field={item.partner_link}
+                  >
+                    <PrismicNextImage
+                      className="mx-auto h-full w-full object-cover"
+                      field={item.partner_logo}
+                    />
+                  </PrismicNextLink>
                 </div>
-              </PrismicNextLink>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
-
-        <div className="mt-12 flex justify-center">
-          <PrismicNextLink field={slice.primary.see_all_button_link}>
-            <button className="flex items-center justify-center w-full max-w-xs mx-auto py-4 px-8 text-white font-bold bg-[#3BBB96] rounded-[16px] hover:bg-[#75DCC0] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#3BBB96] transform transition-all duration-300 ease-in-out lg:w-auto">
-              {slice.primary.see_all_button_label}
-              <ChevronRightIcon className="ml-2 w-5 h-5" />
-            </button>
-          </PrismicNextLink>
-        </div>
+          <div className="mt-12 flex justify-center">
+            <PrismicNextLink field={slice.primary.see_all_button_link}>
+              <button className="flex items-center justify-center w-full max-w-xs mx-auto py-4 px-8 text-[#171717] font-sans font-bold text-base underline underline-offset-8 hover:underline-offset-4 mt-4 transform transition-all duration-300 ease-in-out lg:w-auto">
+                {slice.primary.see_all_button_label}
+              </button>
+            </PrismicNextLink>
+          </div>
       </div>
     </section>
   );
