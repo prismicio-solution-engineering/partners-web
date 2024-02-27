110aa7491d8adb76f1d52e937658416a71702e5b
Dmsooja <dev@sooja.fr>
2024-02-27 19:32:39 +0100

    Display all menu subitems

--- slices/MenuItem/WithSubItemsMenuItem.tsx
+++ slices/MenuItem/WithSubItemsMenuItem.tsx
@@ -71,7 +71,7 @@ const WithSubItemsMenuItem = ({
               leaveFrom="transform opacity-100 scale-100"
               leaveTo="transform opacity-0 scale-95"
             >
-              <Menu.Items className="origin-top-right absolute mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <Menu.Items className="origin-top-right absolute z-10 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                 <div className="py-1">
                   {slice.items.map((item, idx) => (
                     <Menu.Item key={idx}>
