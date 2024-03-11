25d21e7b88760885b048eeabfa6da39a22d0e841
Dmsooja <dev@sooja.fr>
2024-03-11 11:57:05 +0100

    delete single review pages

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -6,16 +6,22 @@ import { components } from "@/slices";
 import { notFound } from "next/navigation";
 import Header from "@/components/Header";
 import { Content } from "@prismicio/client";
+import ReviewLayout from "@/components/ReviewLayout";
 
 export default async function Page() {
   const client = createClient();
   const page = await client.getSingle("reviews").catch(() => notFound());
-  const navigation = await client.getSingle<Content.NavigationDocument>("navigation");
+  const checklist = await client
+    .getSingle("reviews_checklist")
+    .catch(() => notFound());
+  const navigation =
+    await client.getSingle<Content.NavigationDocument>("navigation");
 
   return (
     <>
       <Header navigation={navigation} />
       <SliceZone slices={page?.data?.slices} components={components} />;
+      <ReviewLayout criteria={checklist.data.criteria} />
     </>
   );
 }
