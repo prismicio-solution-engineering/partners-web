ff0201136008fd96e79cdca7c2eaee4d5e0b8b90
Dmsooja <dev@sooja.fr>
2024-03-26 14:09:05 +0100

    Add scroll conatiner

--- slices/FeaturedPartners/index.tsx
+++ slices/FeaturedPartners/index.tsx
@@ -4,6 +4,7 @@ import { SliceComponentProps } from "@prismicio/react";
 import { PrismicRichText } from "@/components/PrismicRichText";
 import { Content, isFilled } from "@prismicio/client";
 import { Button } from "@/components/Button";
+import { ScrollContainer } from "@/components/Articles/ScrollContainer";
 
 /**
  * Props for `FeaturedPartners`.
@@ -17,33 +18,34 @@ export type FeaturedPartnersProps =
 const FeaturedPartners = ({ slice }: FeaturedPartnersProps): JSX.Element => {
   return (
     <section className="bg-white">
-      <div className="flex flex-col items-center my-16 mx-auto text-center max-w-full">
+      <div className="flex flex-col items-center my-16 mx-auto text-center">
         <PrismicRichText field={slice.primary.section_title} />
         {isFilled.richText(slice.primary.section_description) && (
           <PrismicRichText field={slice.primary.section_description} />
         )}
-        <div className="px-4">
+        <div className="max-w-screen-2xl mx-auto relative">
           <div
-            className="flex flex-row gap-8 overflow-x-auto scroll-smooth scrollbar-hide no-scrollbar"
-            style={{ scrollSnapType: "x mandatory" }}
           >
+            <ScrollContainer gap={5}>
             {slice.items.map((item, idx) => (
               <div
                 key={idx}
-                className="flex-none w-1/5 min-w-1/5"
-                style={{ scrollSnapAlign: "start" }}
+                className="shrink-0 h-[100px] w-1/3"
               >
                 <PrismicNextLink
-                  className="block h-24 rounded-lg overflow-hidden cursor-pointer transform hover:scale-110 transition-transform duration-300"
+                  className="block rounded-lg overflow-hidden cursor-pointer transform hover:scale-110 transition-transform duration-300"
                   field={item.partner_link}
                 >
                   <PrismicNextImage
-                    className="mx-auto h-full w-full object-contain"
+                    className="mx-auto object-contain"
+                    width={344}
+                    height={100}
                     field={item.partner_logo}
                   />
                 </PrismicNextLink>
               </div>
             ))}
+            </ScrollContainer>
           </div>
         </div>
         <Button field={slice.primary.see_all_button_link} variant="link">
